f90a706e2a6cc60cba9da92d9ae69f04388aa051
frannav <frannavmart@gmail.com>
2022-05-25 19:33:54 +0100

    Moved AppState enum to other file

---
+++ types/appState.ts
@@ -0,0 +1,10 @@
+/* eslint-disable no-unused-vars */
+// eslint-disable-next-line no-shadow
+export enum AppState {
+  InitialState = "InitialState",
+  PictureAdded = "PictureAdded",
+  Loading = "Loading",
+  FetchingData = "FetchingData",
+  Success = "Success",
+  Error = "Error",
+}
